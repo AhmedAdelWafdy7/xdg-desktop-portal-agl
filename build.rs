3589b7d6a8f70e61e054da6ecc6369f183a62a7a
AhmedAdelWafdy7 <fandes99@gmail.com>
2026-04-05 19:58:18 +0000

    feat: add AGL session environment and configuration files; remove install script

--- build.rs
+++ build.rs
@@ -1,37 +1,4 @@
-use std::env;
-use std::fs;
-use std::path::PathBuf;
-
 fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
     println!("cargo:rerun-if-changed=data/");
     println!("cargo:rerun-if-changed=build.rs");
-    let prefix = env::var("PREFIX").unwrap_or_else(|_| "/usr".to_string());
-    let datadir = env::var("DATADIR")
-        .unwrap_or_else(|_| format!("{}/share", prefix));
-    let libexecdir = env::var("LIBEXECDIR")
-        .unwrap_or_else(|_| format!("{}/libexec", prefix));
-
-    let install_dirs = format!(
-        "pub const PREFIX: &str = \"{}\";\n\
-         pub const DATADIR: &str = \"{}\";\n\
-         pub const LIBEXECDIR: &str = \"{}\";\n",
-        prefix, datadir, libexecdir
-    );
-    fs::write(out_dir.join("install_dirs.rs"), install_dirs).unwrap();
-
-    let service_in = fs::read_to_string("data/xdg-desktop-portal-agl.service.in")
-        .expect("Cannot read service.in");
-
-    let service_out = service_in
-        .replace("@libexecdir@", &libexecdir)
-        .replace("@prefix@", &prefix);
-
-    fs::write(
-        out_dir.join("xdg-desktop-portal-agl.service"),
-        service_out,
-    ).unwrap();
-
-    println!("cargo:rerun-if-changed=data/xdg-desktop-portal-agl.service.in");
 }
---
+++ data/agl-portal.sh
@@ -0,0 +1,4 @@
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+export DBUS_SESSION_BUS_ADDRESS=unix:path=${XDG_RUNTIME_DIR}/bus
+export WAYLAND_DISPLAY=wayland-1
+export XDG_CURRENT_DESKTOP=AGL
