98427fbe4564848fe68afdd59d234c0d53ba4776
AhmedAdelWafdy7 <fandes99@gmail.com>
2026-04-05 22:34:26 +0000

    fix: refactor AGL portal environment variable exports and update service file

--- data/agl-portal.sh
+++ data/agl-portal.sh
@@ -1,3 +1,11 @@
-export XDG_RUNTIME_DIR=/run/user/$(id -u)
-export DBUS_SESSION_BUS_ADDRESS=unix:path=${XDG_RUNTIME_DIR}/bus
-export XDG_CURRENT_DESKTOP=AGL
+if [ -z "${XDG_CURRENT_DESKTOP}" ]; then
+    export XDG_CURRENT_DESKTOP=AGL
+fi
+
+if [ -z "${XDG_RUNTIME_DIR}" ]; then
+    export XDG_RUNTIME_DIR=/run/user/$(id -u)
+fi
+
+if [ -z "${DBUS_SESSION_BUS_ADDRESS}" ]; then
+    export DBUS_SESSION_BUS_ADDRESS=unix:path=${XDG_RUNTIME_DIR}/bus
+fi
